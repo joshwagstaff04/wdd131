bb1af25cff39cd80bec0fed2bc76ef88b6883b06
joshwagstaff04 <joshuawagstaff04@gmail.com>
2026-01-15 11:07:37 -0700

    Delete wdd1312 directory

--- wdd1312/scripts/getdates.js
+++
@@ -1,4 +0,0 @@
-const y = document.getElementById('currentyear');
-if (y) y.textContent = new Date().getFullYear();
-const lm = document.getElementById('lastModified');
-if (lm) lm.textContent = `Last modified: ${document.lastModified}`;
